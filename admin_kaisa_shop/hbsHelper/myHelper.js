584b92e3d405145466d1b0e65e4e35cc4f61bf62
1712916 <kieuphong2840@gmail.com>
2020-01-06 00:53:23 +0700

    Xem danh sách admin, phân trang admin, xem thông tin cá nhân từng thành viên, chỉnh sửa thông tin admin. +Chỉnh sữa về giao diện trang Dang sascg admin, thông tin trên header. +Cấu hình lại các link của xem thông tin cá nhân, chỉnh sửa thông tin cá nhân

--- admin_kaisa_shop/hbsHelper/myHelper.js
+++ admin_kaisa_shop/hbsHelper/myHelper.js
@@ -6,3 +6,53 @@ Handlebars.registerHelper("messageHelper",function(message,options){
   }
 });
 
+
+Handlebars.registerHelper("paginationHelper",function(current,total){
+  var start;
+  var end;
+  var href="/ds_thanh_vien";
+
+  start=current-2;
+  if(start<1){
+      start=1;
+  }
+  end=start+4;
+
+  if(end>total){
+      end=total;
+  }
+  if(end-4>0){
+      start=end-4;
+  }
+
+
+
+  var i;
+  var myString="";
+  var a="<a class="+"\"page-link\""+ "href="+"\"/0\""+">";
+  if(start>1){
+      var pre=current-1;
+      myString=myString+ "<li class="+"\"page-item\""+">"+"<a class="+"\"page-link\""+ "href="+"\""+href+"/"+pre+"\""+">"+"Pre"+"</a>"+"</li>"
+  }
+
+for( i=start;i<=end;i++)
+{
+  if(i==current){
+      myString=myString+ "<li class="+"\"page-item active\""+">"+"<a class="+"\"page-link\""+ "href="+"\""+href+"/"+i+"\""+">"+i+"</a>"+"</li>"
+  }else{
+      myString=myString+ "<li class="+"\"page-item\""+">"+"<a class="+"\"page-link\""+ "href="+"\""+href+"/"+i+"\""+">"+i+"</a>"+"</li>"
+  }
+ 
+}
+
+if(total>end){
+  
+  const next=1+Number(current);
+  myString=myString+ "<li class="+"\"page-item\""+">"+"<a class="+"\"page-link\""+ "href="+"\""+href+"/"+next+"\""+">"+"Next"+"</a>"+"</li>"
+}
+console.log(myString);
+//return new Handlebars.SafeString("<li class="+"\"page-item\""+">"+a+"Previous"+"</a>"+"</li>");
+return new Handlebars.SafeString(myString);
+
+
+});
--- admin_kaisa_shop/routes/index.js
+++ admin_kaisa_shop/routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const Passport=require('passport');
 const Admin=require('../models/admin');
+const Products=require('../models/sanPham');
 
 
 
@@ -23,7 +24,7 @@ router.get('/',function(req,res,next){
     res.redirect('/dang_nhap');
   }
  
-  res.render('index',{title: 'kaisa Shop', profile:req.user});
+  res.render('index',{title: 'kaisa Shop', headProfile:req.user});
 });
 
 router.get('/dang_ky', function(req, res, next) {
@@ -83,25 +84,99 @@ router.get('/ds_thanh_vien', function(req, res, next) {
   if(!req.isAuthenticated()){
     res.redirect('/dang_nhap');
   }
-  res.render('./thanh_vien/danh_sach_thanh_vien', { title: 'Express' });
+  res.redirect('/ds_thanh_vien/1');
 });
+
+router.get('/ds_thanh_vien/:page', function(req, res, next) {
+  if(!req.isAuthenticated()){
+    res.redirect('/dang_nhap');
+  }
+  var perPage = 4;
+  var page = req.params.page || 1;
+  
+  // const search={
+  //   tenSanPham:"Dell",
+  //   giaSanPham:999000
+  // };
+
+  
+  var skip=(page-1)*perPage;
+
+  Admin.find({}).skip(skip).limit(perPage).exec((err,data)=>{
+    if(!err){
+        Admin.count().exec((err,count)=>{
+          if(!err){
+            res.render('./thanh_vien/danh_sach_thanh_vien', { title: 'Danh sách thành viên',
+                                listAdmin:data,
+                                current: page,
+                                total: Math.ceil(count/perPage),
+                                headProfile:req.user});
+          }
+        })
+    }
+
+    
+
+  })
+  
+
+});
+
+
 router.get('/ds_don_hang', function(req, res, next) {
   if(!req.isAuthenticated()){
     res.redirect('/dang_nhap');
   }
-  res.render('./don_hang/danh_sach_don_hang', { title: 'Express' });
+  res.render('./don_hang/danh_sach_don_hang', { title: 'Express' ,headProfile:req.user});
 });
 
 
 
+
+
+router.get('/profile', function(req, res, next) {
+  if(!req.isAuthenticated()){
+    res.redirect('/dang_nhap');
+  }
+
+  res.render('./thanh_vien/ds_thanh_vien/thanh_vien_1', { title: 'KaisaAdmin',profile:req.user,headProfile:req.user });
+});
 router.get('/ds_thanh_vien/thanh_vien_1', function(req, res, next) {
   if(!req.isAuthenticated()){
     res.redirect('/dang_nhap');
   }
-  res.render('./thanh_vien/ds_thanh_vien/thanh_vien_1', { title: 'KaisaAdmin',profile:req.user });
+
+  res.render('./thanh_vien/ds_thanh_vien/thanh_vien_1', { title: 'KaisaAdmin',profile:req.user,headProfile:req.user });
 });
 
-router.post('/update_profile',function(req,res,next){
+router.get('/ds_thanh_vien/thanh_vien_1/:_id', function(req, res, next) {
+  if(!req.isAuthenticated()){
+    res.redirect('/dang_nhap');
+  }
+  var id=req.params._id;
+  Admin.findOne({_id:id}).exec((err,data)=>{
+    if(err){
+      res.redirect('/');
+    }
+    
+    // const profileAdmin={
+    //   email:data.email,
+    //   name:data.name,
+    //   phone:data.phone,
+    //   birthday: data.birthday,
+    //   address:data.address,
+    //   company: data.company
+    // }
+   // console.log(profileAdmin);
+    res.render('./thanh_vien/ds_thanh_vien/thanh_vien_1', { title: 'KaisaAdmin',profile:data,headProfile:req.user });
+
+  })
+
+  
+});
+
+
+router.post('/update_profile/:_id',function(req,res,next){
   
 
   const update = { name: req.body.name,
@@ -111,9 +186,9 @@ router.post('/update_profile',function(req,res,next){
                   company: req.body.company };
                   
   
-  const id = req.user._id;
+  const id = req.params._id;
 
-    const admin=Admin.findByIdAndUpdate(id,update,function(err,res){
+    Admin.findByIdAndUpdate(id,update,function(err,res){
         if(err){
           res.send("Thay đổi không thành công!");
           res.redirect('/');
@@ -122,22 +197,65 @@ router.post('/update_profile',function(req,res,next){
     } )
 
 
-
-   res.redirect('/ds_thanh_vien/thanh_vien_1');
+    if(id==req.user._id){
+      res.redirect('/profile');
+    }
+    res.redirect('/ds_thanh_vien/thanh_vien_1/'+id); 
+  
 })
 
 router.get('/top_san_pham', function(req, res, next) {
   if(!req.isAuthenticated()){
     res.redirect('/dang_nhap');
   }
-  res.render('./san_pham/top_san_pham', { title: 'Express' });
+  res.render('./san_pham/top_san_pham', { title: 'Express',headProfile:req.user });
 });
 router.get('/tat_ca_san_pham', function(req, res, next) {
   if(!req.isAuthenticated()){
     res.redirect('/dang_nhap');
   }
-  res.render('./san_pham/tat_ca_san_pham', { title: 'Express' });
+  
+  Products.find().exec((err,data)=>{
+    if(err){
+      res.redirect('/');
+    }
+    console.log("this is my data:" +data);
+  res.render('./san_pham/tat_ca_san_pham', { title: 'Danh sách sản phẩm', listProduct:data ,headProfile:req.user});
+  })
+
+  
 });
+
+// router.get('/:page', function(req, res, next) {
+//   var perPage = 2
+//   var page = req.params.page || 1;
+  
+//   const search={
+//     tenSanPham:"Dell",
+//     giaSanPham:999000
+//   };
+
+  
+//   var skip=(page-1)*perPage;
+
+//   Admin.find({}).skip(skip).limit(perPage).exec((err,data)=>{
+//     if(!err){
+//         Admin.count().exec((err,count)=>{
+//           if(!err){
+//             res.render('index', { title: 'Express',
+//                                 content:data,
+//                                 current: page,
+//                                 total: Math.ceil(count/perPage)});
+//           }
+//         })
+//     }
+
+    
+
+//   })
+  
+
+// });
 
 
 
